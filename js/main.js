12e00b2f9c89c856bd6dee2b17fff1e3d63d5b91
M.angel <mg20101994@gmail.com>
2019-01-30 19:18:11 -0500

    finish proyect

---
+++ js/main.js
@@ -0,0 +1,7 @@
+'use strict'
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $("#mainNavbar");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    })
+});
